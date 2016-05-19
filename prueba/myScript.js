aa202e40845daa97262f24566a3fc9dc52d8e205
NoelGaspar <waaraya@uc.cl>
2016-05-19 19:47:26 -0300

    Add files via upload

---
+++ prueba/myScript.js
@@ -0,0 +1,43 @@
+function myBoton() {
+	//document.getElementById('demo').innerHTML="cosaASDASD";
+	//window.confirm("cupalo");
+	//window.alert("cupalo");
+
+
+}
+
+var D=0;
+var pos=200;
+function der(){
+	var image = document.getElementById('myImage');
+  	if(D=0)
+  		{image.src='der.gif';
+  		D=1;}
+  	else if(D=1)	
+  	{
+  		D=1;}
+  	if(D=-1)
+  		{image.src='der.gif';
+  		D=1;}
+  	
+   	pos=pos+10;
+   	if(pos>=400){pos=400;}
+   	image.style.left=pos+'px';
+    document.getElementById('demo').innerHTML=pos;
+
+}
+
+function izq(){
+	var image = document.getElementById('myImage');
+	if(D=0)
+    {image.src='izq.gif';D=-1;}
+  else if(D=-1)
+    {D=-1;}
+  if(D=1)
+    {image.src='izq.gif';D=-1;}
+  
+  pos=pos-10;
+  if(pos<=0){pos=0;}
+  image.style.left=pos+'px';
+  document.getElementById('demo').innerHTML=pos;
+}
